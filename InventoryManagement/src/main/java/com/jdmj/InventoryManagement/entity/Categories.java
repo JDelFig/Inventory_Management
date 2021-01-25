4aa8b7b4ee6bd9fb5175c97d19b71ba4de2b0a72
JDelFig <55452601+JDelFig@users.noreply.github.com>
2021-01-25 12:40:35 -0500

    Created Entities Craeted Hibernate Config Added Dependencies Mapped Entities

---
+++ InventoryManagement/src/main/java/com/jdmj/InventoryManagement/entity/Categories.java
@@ -0,0 +1,10 @@
+package com.jdmj.InventoryManagement.entity;
+
+public enum Categories {
+
+	TOOLS,
+	FOOD,
+	HEALTH,
+	GAMES,
+	OTHER
+}
---
+++ InventoryManagement/src/main/java/com/jdmj/InventoryManagement/entity/Product.java
@@ -0,0 +1,83 @@
+package com.jdmj.InventoryManagement.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	@Column(name="name")
+	private String name;
+	@Column(name="category")
+	private String category;
+	@Column(name="quantity")
+	private int quantity;
+	
+	@OneToOne()  
+	@JoinColumn(name="product_info_id")
+	private ProductInfo productInfo;
+
+	public Product() {
+
+	}
+
+	public Product(String name, String category, int quantity, ProductInfo productInfo) {
+		this.name = name;
+		this.category = category;
+		this.quantity = quantity;
+		this.productInfo = productInfo;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public ProductInfo getProductInfo() {
+		return productInfo;
+	}
+
+	public void setProductInfo(ProductInfo productInfo) {
+		this.productInfo = productInfo;
+	}
+
+}
